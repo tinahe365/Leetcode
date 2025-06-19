4cab9dc963f5c8f84b5e4206b40c64325bd7e795
Tina He <tinahe365@gmail.com>
2025-06-19 16:22:20 +1000

    findDifference btw two arrays

---
+++ 2215_findDifference.py
@@ -0,0 +1,15 @@
+from typing import List
+def findDifference(nums1: List[int], nums2: List[int]) -> List[List[int]]:
+    ans0 = set(nums1)
+    ans1 = set(nums2)
+    
+    # common_nums = ans0.intersection(ans1)
+
+    # for item in common_nums:
+    #     ans0.remove(item)
+    #     ans1.remove(item)
+    
+    # return ([list(ans0),list(ans1)])
+
+    return [list(ans0 - ans1), list(ans1 - ans0)] # This is a more concise way to find the difference
+
